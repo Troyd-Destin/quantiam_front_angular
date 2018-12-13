fadbbd0df72843745eef9d76ae4a0d429e2a0dbc
Gogs <gogs@fake.local>
2018-12-13 12:01:36 -0700

    fixes

--- src/CHANGELOG.md
+++ src/CHANGELOG.md
@@ -5,7 +5,7 @@
    **[Upgrade]** Updated from Angular 7.0 to 7.1  
 
 #### General
-   **[Feature]** Implmented basic caching for named route, this means that when users are navigating to and fro a table, the settings, filters and searches will be saved.  
+   **[Feature]** Implmented basic caching for named routes, this means that when users are navigating to and fro a table, the settings, filters and searches will be saved.  
 	
 #### User
    **[Feature]** RTO Allotment can now be viewed from the user screen  
--- src/app/reuse-strategy.ts
+++ src/app/reuse-strategy.ts
@@ -2,27 +2,27 @@ import {ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy} from '@
 
 export class CustomReuseStrategy implements RouteReuseStrategy {
 
-  routesToCache: string[] = ["userDatabase","userView","xpsDatabase"];
+  routesToCache: string[] = ['userDatabase','userView','xpsDatabase'];
   storedRouteHandles = new Map<string, DetachedRouteHandle>();
 
   // Decides if the route should be stored
   shouldDetach(route: ActivatedRouteSnapshot): boolean {
-     return this.routesToCache.indexOf(route.data["key"]) > -1;
+     return this.routesToCache.indexOf(route.data['key']) > -1;
   }
 
   //Store the information for the route we're destructing
   store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-     this.storedRouteHandles.set(route.data["key"], handle);
+     this.storedRouteHandles.set(route.data['key'], handle);
   }
 
   //Return true if we have a stored route object for the next route
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
-     return this.storedRouteHandles.has(route.data["key"]);
+     return this.storedRouteHandles.has(route.data['key']);
   }
 
   //If we returned true in shouldAttach(), now return the actual route data for restoration
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-     return this.storedRouteHandles.get(route.data["key"]);
+     return this.storedRouteHandles.get(route.data['key']);
   }
 
   //Reuse the route if we're going to and from the same route
