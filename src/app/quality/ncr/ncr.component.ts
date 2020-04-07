d2164444092e6b060a2b22d98cd8c5ae331f0113
Gogs <gogs@fake.local>
2020-04-07 10:45:26 -0600

    2020-04-07

--- src/app/quality/ncr/ncr.component.ts
+++ src/app/quality/ncr/ncr.component.ts
@@ -1,7 +1,7 @@
 
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {Component, ElementRef, ViewChild,OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup,} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup,Validators} from '@angular/forms';
 import {MatChipInputEvent} from '@angular/material/chips';
 import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
 import { UserService } from '../../services/user/user.service';
@@ -15,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { NotificationsService } from 'angular2-notifications';
+import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
 
 export interface Fruit {
   name: string;
@@ -28,26 +29,20 @@ export interface Fruit {
 export class NcrComponent implements OnInit {
 
   
-  @ViewChild('userInput') userInput: ElementRef<HTMLInputElement>;
+  @ViewChild('responsibleInput') responsibleInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
-  ncrOptions = new FormGroup({
-    name: new FormControl(),
-    requirement_violated: new FormControl(),
-    type: new FormControl(),
-    severity: new FormControl(),
-    description: new FormControl(),
-    immediate_containment_action: new FormControl(),
-    buisness_unit: new FormControl(),
-    status: new FormControl(),
-    project_id: new FormControl(),
-    responsible: new FormControl(),
-   });
+  ncrFiles: NgxFileDropEntry[] = [];
   ncrName;
-  ncrObject = {id:null, status: null};
+  ncrOptions;
+  ncrObject = {
+    id:null, status: null, name: null, requirement_violated:null,
+    type:null,severity:null,description:null,buisness_unit:null,project_id:null,occurred:null
+  };
   ncrLoading = false;
 
   showNcrDocumentUpload = false;
+  showCarDocumentUpload = false; 
 
   projectList;
   filteredProjects: Observable<any[]>;
@@ -60,7 +55,7 @@ export class NcrComponent implements OnInit {
   removable = true;
   addOnBlur = true;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
-  selectedResponsible = [
+  selectedResponsible = [{id:65,name:'Tyson Boyce',title:'QA/QC Supervisor', clearable: false}
   ];
 
   allUsers;  
@@ -78,6 +73,24 @@ export class NcrComponent implements OnInit {
    }
 
   ngOnInit(): void {
+
+    this.ncrOptions = new FormGroup({
+      name: new FormControl(this.ncrObject.name, [Validators.required]),
+      requirement_violated: new FormControl(this.ncrObject.requirement_violated, [Validators.required]),
+      type: new FormControl(this.ncrObject.type, [Validators.required]),
+      severity: new FormControl(this.ncrObject.severity, [Validators.required]),
+      description: new FormControl(this.ncrObject.description, [Validators.required]),
+      immediate_containment_action: new FormControl(null,null),
+      buisness_unit: new FormControl(this.ncrObject.buisness_unit, [Validators.required]),
+      status: new FormControl(this.ncrObject.status,null),
+      project_id: new FormControl(this.ncrObject.project_id,null),
+      occurred: new FormControl(this.ncrObject.occurred,[Validators.required]),
+      responsible: new FormControl(this.selectedResponsible,[Validators.required]),
+     });
+
+     this.ncrOptions.controls['responsible'].setValue({id:65,name:'Tyson Boyce',title:'QA/QC Supervisor', clearable: false});
+
+
   
     this.ncrLoading = true;
 
@@ -118,7 +131,7 @@ export class NcrComponent implements OnInit {
                {
                const filteredArray = this.userList.filter((item)=>{
                 //  console.log(item.name.includes(value));
-                 if(item.name.toLowerCase().includes(value) || item.name.includes(value)) { return item; }
+                 if((item.name.toLowerCase().includes(value) || item.name.includes(value)) && item.active) { return item; }
                })               
                return filteredArray;
               }
@@ -138,50 +151,93 @@ export class NcrComponent implements OnInit {
 
         this.ncrObject = r;
         this.ncrOptions.patchValue(r);
+        this.selectedResponsible = [];
+        r.responsible.forEach(element => {
+            this.selectedResponsible.push(element.employee);
+        });
+
+        this.ncrOptions.updateValueAndValidity();
         this.ncrLoading = false;
 
     })
 
   }
 
-  updateNCR()
-  { 
-      this.http.put(environment.apiUrl + '/ncr/' + this.ncrObject.id, this.ncrOptions.getRawValue()).subscribe((r)=>{
+  updateNCR(status)
+  {     
+      
+      let params =  this.ncrOptions.getRawValue();
+      if(status){ params.status = status; }
+      params.responsible = this.selectedResponsible;
+      this.http.put(environment.apiUrl + '/ncr/' + this.ncrObject.id, params).subscribe((r)=>{
         this.notification.success('Success',  'NCR Details Saved', {timeOut: 4000, showProgressBar: false, clickToClose: true}); /// Daily OT notificaton
       }) 
   }
 
-  add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
-    console.log(event);
-
-    // Add our fruit
-    if ((value || '').trim()) {
-      this.selectedResponsible.push(value);
-    }
-
+  selectResponsible(event)
+  {
+   
+    this.selectedResponsible.push(event.option.value);    
+    this.responsibleInput.nativeElement.value = '';
     console.log(this.selectedResponsible);
-
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
   }
 
-  remove(fruit: Fruit): void {
-    const index = this.selectedResponsible.indexOf(fruit);
+  add(event: MatChipInputEvent): void {
+    console.log(event);
+    this.ncrOptions.controls['responsible'].setValue(null);    
+    this.responsibleInput.nativeElement.value = '';
+  }
 
+  remove(event,index): void {
+    console.log(event,index);
     if (index >= 0) {
       this.selectedResponsible.splice(index, 1);
     }
   }
 
-  selected(event: MatAutocompleteSelectedEvent): any {
-    this.selectedResponsible.push({name:'test'});
-    this.userInput.nativeElement.value = '';
+
+  checkEditPrivledges()
+  {
+
+
   }
 
+
+  NCRdroppedfile(files: NgxFileDropEntry[]){
+    this.ncrFiles = files;
+    console.log(files);
+    for (const droppedFile of files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+          /**
+          // You could upload it like this:
+           **/
+          const formData = new FormData()
+          formData.append('media', file, droppedFile.relativePath)
+  
+          this.http.put(environment.apiUrl + '/ncr/' + this.ncrObject.id, formData, { responseType: 'blob' })
+          .subscribe(data => {
+              console.log(data);
+            // Sanitized logo returned from backend
+          })
+         
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }}
+  NCRfileOver(event){ console.log(event);}
+  NCRfileLeave(event){ console.log(event);}
 
 
 }
